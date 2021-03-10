f8e9b2e69ecd6ccbf1e4aa581336fd33a552b865
const-ntino <constantinojoaopedro1@gmail.com>
2021-03-10 16:07:08 -0300

    111. Minimum Depth of Binary Tree

--- 111.cpp
+++ 111.cpp
@@ -1,6 +1,6 @@
 // 111. Minimum Depth of Binary Tree
-// Given a binary tree, find its m_depth depth.
-// The m_depth depth is the number of nodes along the shortest path from the
+// Given a binary tree, find its minimum depth.
+// The minimum depth is the number of nodes along the shortest path from the
 // root node down to the nearest leaf node.
 
 #include <bits/stdc++.h>
